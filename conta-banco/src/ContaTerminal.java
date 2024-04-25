2bafce063871d8fa7af58caf40c855a169ba60a8
Guilherme Ganzarolli <guilhermegalmeida@hotmail.com>
2024-04-25 18:49:40 -0300

    Programa realizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite o número da sua agência:");
+        int nAgencia = scanner.nextInt();
+
+        System.out.println("Agora digite o número da sua conta:");
+        int nConta = scanner.nextInt();
+
+        System.out.println("Digite quanto você tem na sua conta");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+nAgencia+", conta "+nConta+" e seu saldo "+ saldo+ 
+        " já está disponível para saque.");
     }
 }
